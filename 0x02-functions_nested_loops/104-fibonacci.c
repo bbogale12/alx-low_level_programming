b325906ebddfef19d3af727eb741068c99a139f4
bbogale12 <bbogale12@gmail.com>
2022-04-12 16:52:51 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * Main - prints the first 98 Fibonacci numbers starting with 
- *        1 and 2 followed by a new line. 
+ * main - prints the first 98 Fibonacci numbers, starting with 
+ *        1 and 2, followed by a new line. 
  *
  * Return: Always 0.
  */
