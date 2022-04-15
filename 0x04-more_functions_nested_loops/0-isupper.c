c5b45e8225425177330987e5d193010d1f3d65ef
bbogale12 <bbogale12@gmail.com>
2022-04-15 14:56:19 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _isupper - Check upper case
+  * @c: print int
+  *
+  * Return: give 1 or 0
+  */
+
+int _isupper(int c)
+{
+if (c >= 'A' && C <= 'Z')
+{
+return (1);
+}
+
+return (0);
+}
+
