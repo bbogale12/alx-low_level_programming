a6ab80b6511a964132cc0fc714fd4a4faac54a1d
Bereketab Bogale <bbogale12@gmail.com>
2022-07-11 13:45:17 +0300

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN__
+#define __MAIN__
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +21,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+int nrandom();
+
+
+#endif /* __MAIN__ */
